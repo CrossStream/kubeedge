5521ceb813574f1ba0ff940236743eb703760b29
Philippe Coval <p.coval@samsung.com>
2019-10-31 13:16:52 +0100

    keadm: Install to system managed dir
    Change-Id: I50cc8df471758da84d998c27b8ecfc0fd33ffa8d
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- keadm/app/cmd/util/common.go
+++ keadm/app/cmd/util/common.go
@@ -43,7 +43,7 @@ const (
 
 	KubeEdgeDownloadURL       = "https://github.com/kubeedge/kubeedge/releases/download"
 	KubeEdgePath              = "/etc/kubeedge/"
-	KubeEdgeUsrBinPath        = "/usr/local/bin"
+	KubeEdgeUsrBinPath        = "/usr/bin"
 	KubeEdgeConfPath          = KubeEdgePath + "kubeedge/edge/conf"
 	KubeEdgeBinaryName        = "edgecore"
 	KubeEdgeDefaultCertPath   = KubeEdgePath + "certs/"
